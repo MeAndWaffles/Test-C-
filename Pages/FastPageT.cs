b884948102c48b36efab8ba868c436905020d132
Vlad <bwaffle65@gmail.com>
2023-04-11 16:11:20 +0300

    Basis

---
+++ Pages/FastPageT.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Test_C_.Pages
+{
+    internal class FastPageT : BasePage
+    {
+        public FastPageT(IWebDriver driver) : base(driver)
+        {
+
+
+        }
+        
+        public WebElement someElement()
+        {
+            return findElement("");
+        }
+    }
+}
---
+++ Pages/HomePage.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+
+namespace Test_C_.Pages
+{
+    internal class HomePage : BasePage
+    {
+        private readonly By searchInput = By.Name("q");
+
+        public HomePage(IWebDriver driver) : base(driver)
+        {
+            driver.Navigate().GoToUrl("https://www.google.com/");
+        }
+
+        public SearchResultPage SearchFor(string query)
+        {
+            Type(searchInput, query + Keys.Enter);
+            return new SearchResultPage(driver);
+        }
+    }
+}
